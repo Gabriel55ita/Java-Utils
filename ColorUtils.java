6d73cc2616cf03accce89ff47fa8cf0d927f8a6b
Gabriel55ita <60621266+Gabriel55ita@users.noreply.github.com>
2022-03-15 15:29:03 +0100

    Create ColorUtils.java
    It's a utility i used in my mc client.

---
+++ ColorUtils.java
@@ -0,0 +1,37 @@
+import java.awt.*;
+
+public class ColorUtils {
+
+    /**
+     * Converts hex color value into RGB
+     * @param hex string (including the #)
+     * @return RGB value from the hex
+     */
+    public static Color hexToRGB(String hex) {
+        return new Color(
+                Integer.valueOf(hex.substring(1, 3), 16),
+                Integer.valueOf(hex.substring(3, 5), 16),
+                Integer.valueOf(hex.substring(5, 7), 16)
+        );
+    }
+
+    /**
+     * Converts RGB color value into hex
+     * @param color
+     * @return hex string
+     *
+     * Found in Stackoverflow a while ago
+     */
+    public static String rgbToHex(Color color) {
+        String hex = String.format("#%06x", Integer.valueOf(color.getRGB() & 0x00FFFFFF));
+        return hex;
+    }
+
+    /**
+     * @return cool Rainbow animated color
+     */
+    public static int getRainbow() {
+        final long time = System.currentTimeMillis();
+        return Color.HSBtoRGB(time % 2000L / 2000.0f, 0.8f, 0.8f);
+    }
+}
